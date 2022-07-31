773a86d77f7b28612cddb89820a49c1254edf9a3
Dominik Heller <dominik.heller1@gmail.com>
2022-07-31 13:55:56 +0200

    Fill EditPieceActivity with actual data of the piece (even if modifications cannot be stored yet)

--- app/src/main/java/com/example/playmenext/EditPieceActivity.kt
+++ app/src/main/java/com/example/playmenext/EditPieceActivity.kt
@@ -14,36 +14,48 @@ import com.example.playmenext.domain.PieceToPractice
 const val EXTRA_PIECE_INST = "com.example.playmenext.PIECE_INST"
 
 class EditPieceActivity : AppCompatActivity() {
-    private lateinit var editTitleView: EditText
+    private lateinit var _editTitleView: EditText
+    private lateinit var _editSubtitleView: EditText
+    private lateinit var _editComposerView: EditText
+    private lateinit var _editArrangerView: EditText
+    private lateinit var _editPriorityView: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_piece)
 
-        editTitleView = findViewById(R.id.edit_title)
-
-        val repo = (application as MainApplication).repository
-
+        _editTitleView = findViewById(R.id.edit_title)
+        _editSubtitleView = findViewById(R.id.edit_subtitle)
+        _editComposerView = findViewById(R.id.edit_composer)
+        _editArrangerView = findViewById(R.id.edit_arranger)
+        _editPriorityView = findViewById(R.id.edit_priority)
 
         val existingPiece = intent.getParcelableExtra<PieceToPractice>(EXTRA_PIECE_INST)
         if(existingPiece != null) {
-            Toast.makeText(this,
-                existingPiece.title ?: "Invalid entry!", Toast.LENGTH_SHORT).show()
+            applyData(existingPiece)
         }
 
         val button = findViewById<Button>(R.id.button_save)
         button.setOnClickListener{
             val replyIntent = Intent()
-            if(TextUtils.isEmpty(editTitleView.text)) {
+            if(TextUtils.isEmpty(_editTitleView.text)) {
                 setResult(Activity.RESULT_CANCELED, replyIntent)
             }
             else {
-                val word = editTitleView.text.toString()
+                val word = _editTitleView.text.toString()
                 replyIntent.putExtra(EXTRA_REPLY, word)
                 setResult(Activity.RESULT_OK, replyIntent)
             }
             finish()
         }
+    }
+
+    private fun applyData(pieceData: PieceToPractice) {
+        _editTitleView.setText(pieceData.title)
+        _editSubtitleView.setText(pieceData.subTitle)
+        _editComposerView.setText(pieceData.composer)
+        _editArrangerView.setText(pieceData.arranger)
+        _editPriorityView.setText(pieceData.practicePriority.toString())
     }
 
     companion object {
